c8d5c911d77de4acbcece1fce4fc2b4fa8226e1a
Idada Osaigbovo James <wijojames@gmail.com>
2025-02-08 20:28:29 +0100

    feat: reading the file from the input text to complete day 2 part 1

--- day2/main.go
+++ day2/main.go
@@ -1,10 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
+
+	// Read file content
+	data, err := os.ReadFile("day2-input.txt")
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+
+	// Process file content into slices of integers
+	valued := parseData(string(data))
 	safe := 0
-	valued := [][]int{{1, 3, 6, 7, 9}, {8, 6, 4, 4, 1}, {1, 3, 2, 4, 5}, {9, 7, 6, 2, 1}, {1, 2, 7, 8, 9}, {7, 6, 4, 2, 1}}
+	//valued := [][]int{{1, 3, 6, 7, 9}, {8, 6, 4, 4, 1}, {1, 3, 2, 4, 5}, {9, 7, 6, 2, 1}, {1, 2, 7, 8, 9}, {7, 6, 4, 2, 1}}
 	for _, v := range valued {
 		//Check if all values are being incremented or decremented
 		if allIncreasing(v) || allDecreasing(v) {
@@ -45,4 +60,28 @@ func allDecreasing(values []int) bool {
 		}
 	}
 	return checker
+}
+
+// Parse the file content into [][]int
+func parseData(data string) [][]int {
+	lines := strings.Split(strings.TrimSpace(data), "\n")
+	var valued [][]int
+
+	for _, line := range lines {
+		numStrs := strings.Fields(line) // Split line into numbers
+		var nums []int
+
+		for _, numStr := range numStrs {
+			num, err := strconv.Atoi(numStr) // Convert string to int
+			if err != nil {
+				fmt.Println("Error converting number:", err)
+				continue
+			}
+			nums = append(nums, num)
+		}
+
+		valued = append(valued, nums)
+	}
+
+	return valued
 }
