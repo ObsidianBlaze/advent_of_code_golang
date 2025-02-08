6ee69a9426ad5406699910853d51afc764848bf5
Idada Osaigbovo James <wijojames@gmail.com>
2025-02-08 22:52:56 +0100

    feat: solving the part two to get more safe reports

--- day2/main.go
+++ day2/main.go
@@ -8,7 +8,6 @@ import (
 )
 
 func main() {
-
 	// Read file content
 	data, err := os.ReadFile("day2-input.txt")
 	if err != nil {
@@ -19,47 +18,74 @@ func main() {
 	// Process file content into slices of integers
 	valued := parseData(string(data))
 	safe := 0
-	//valued := [][]int{{1, 3, 6, 7, 9}, {8, 6, 4, 4, 1}, {1, 3, 2, 4, 5}, {9, 7, 6, 2, 1}, {1, 2, 7, 8, 9}, {7, 6, 4, 2, 1}}
+	//valued := [][]int{
+	//	{1, 3, 6, 7, 9},
+	//	{8, 6, 4, 4, 1},
+	//	{1, 3, 2, 4, 5},
+	//	{9, 7, 6, 2, 1},
+	//	{1, 2, 7, 8, 9},
+	//	{7, 6, 4, 2, 1},
+	//}
+
 	for _, v := range valued {
-		//Check if all values are being incremented or decremented
-		if allIncreasing(v) || allDecreasing(v) {
-			safe += 1
+		// Check if the report is already safe
+		if isSafe(v) {
+			safe++
+			continue
+		}
+
+		// Try removing one element at a time and check if it becomes safe
+		for i := 0; i < len(v); i++ {
+			temp := append([]int{}, v[:i]...) // Copy elements before index i
+			temp = append(temp, v[i+1:]...)   // Copy elements after index i
+
+			if isSafe(temp) {
+				safe++
+				break
+			}
 		}
 	}
-	fmt.Println(safe)
 
+	fmt.Println(safe)
+}
+
+// Check if a sequence is safe (either all increasing or all decreasing)
+func isSafe(values []int) bool {
+	return allIncreasing(values) || allDecreasing(values)
 }
 
 func allIncreasing(values []int) bool {
-	checker := false
+	if len(values) < 2 {
+		return false
+	}
+
 	for i := 0; i < len(values)-1; i++ {
-		//Check if it is all increasing
-		if values[i] > values[i+1] {
-			checker = true
-			if values[i]-values[i+1] > 3 {
-				return false
-			}
-		} else {
+		diff := values[i+1] - values[i]
+		if diff < 1 || diff > 3 { // Difference must be between 1 and 3
+			return false
+		}
+		if values[i] >= values[i+1] { // Must always increase
 			return false
 		}
 	}
-	return checker
+	return true
 }
 
 func allDecreasing(values []int) bool {
-	checker := false
+	if len(values) < 2 {
+		return false
+	}
+
 	for i := 0; i < len(values)-1; i++ {
-		//Check if it is all decreasing
-		if values[i] < values[i+1] {
-			checker = true
-			if values[i+1]-values[i] > 3 {
-				return false
-			}
-		} else {
+		diff := values[i] - values[i+1]
+		if diff < 1 || diff > 3 { // Difference must be between 1 and 3
+			return false
+		}
+		if values[i] <= values[i+1] { // Must always decrease
 			return false
 		}
 	}
-	return checker
+	return true
 }
 
 // Parse the file content into [][]int
